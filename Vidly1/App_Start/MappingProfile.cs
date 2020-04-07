7bb36467d1813665cd0ba0a79f92c939c211c4f3
Sikiru Olatunji <Sikiru.Olatunji@wemabank.com>
2020-04-07 13:48:25 +0100

    Correction on mapping profile

--- Vidly1/App_Start/MappingProfile.cs
+++ Vidly1/App_Start/MappingProfile.cs
@@ -15,7 +15,7 @@ namespace Vidly1.App_Start
 			Mapper.CreateMap<Customer, CustomerDto>();
 			Mapper.CreateMap<CustomerDto, Customer>();
 			Mapper.CreateMap<Movie, MovieDto>();
-			Mapper.CreateMap<Movie, MovieDto>();
+			Mapper.CreateMap<MovieDto, Movie>();
 		}
 	}
 }
